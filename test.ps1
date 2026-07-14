e51183db927b74f6f574ac901049beee8ebdcf8f
taylornrolyat <taylortriggs@gmail.com>
2026-07-14 09:34:22 -0600

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,5 @@
-start-process 'c:\windows\system32\calc.exe'
-invoke-webrequest "https://downloads.rclone.org/v1.74.4/rclone-v1.74.4-windows-amd64.zip" -outfile "$env:TEMP\rclone.zip"
-expand-archive -path "$env:TEMP\rclone.zip" -DestinationPath "$env:TEMP\rclone" -Force
+Start-Process "C:\Windows\System32\calc.exe"
+Invoke-WebRequest "https://downloads.rclone.org/v1.74.4/rclone-v1.74.4-windows-amd64.zip" -OutFile "$env:TEMP\rclone.zip"
+Expand-Archive -Path "$env:TEMP\rclone.zip" -DestinationPath "$env:TEMP\rclone" -Force
+$rclone = Get-ChildItem "$env:TEMP\rclone" -Filter "rclone.exe" -Recurse | Select-Object -ExpandProperty FullName -First 1
+& $rclone copyurl "https://secure.eicar.org/eicar.com.txt" "$env:TEMP\eicar.com.txt"
